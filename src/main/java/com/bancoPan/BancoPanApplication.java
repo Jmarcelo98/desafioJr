caba89b67f9eb28aef3ffc8a1908c6c06b4e7a01
Joao Marcelo <joaomarcelo588@gmail.com>
2021-06-22 16:35:30 -0300

    Created project and swagger add/configured

---
+++ src/main/java/com/bancoPan/BancoPanApplication.java
@@ -0,0 +1,13 @@
+package com.bancoPan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoPanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoPanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancoPan/BancoPanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoPan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoPanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
